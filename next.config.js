f8e20f73e0da26e8348285057a6a41f6da546757
thelordofclaude <thelordofclaude@gmail.com>
2026-09-19 12:43:32 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
   images: { unoptimized: true },
   trailingSlash: true,
-}
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
